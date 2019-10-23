e407f3cd6f0d43c84de0444d6449c485e3626250
burakkorkmaz97 <burakkorkmaz.bilkent@gmail.com>
2019-10-23 14:17:39 +0300

    chromedriver works at background

--- find_sol.py
+++ find_sol.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
+import os
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+browser = webdriver.Chrome(os.path.abspath(os.curdir)+'/chromedriver',options=chrome_options)
 
-browser = webdriver.Chrome('/Users/burakkorkmaz/Desktop/4 - 1/cs 461 - ai/project/implementation/chromedriver')
 browser.get('https://www.nytimes.com/crosswords/game/mini')
 browser.find_elements_by_xpath('//*[@id="root"]/div/div/div[4]/div/main/div[2]/div/div[2]/div[2]/article/div[2]/button/div')[0].click()
 browser.find_elements_by_xpath('//*[@id="root"]/div/div/div[4]/div/main/div[2]/div/div/ul/div[2]/li[2]/button')[0].click()
@@ -15,12 +18,11 @@ clues_down = []
 clues_across.append(browser.find_elements_by_xpath('//*[@id="root"]/div/div/div[4]/div/main/div[2]/div/article/section[2]/div[1]/ol')[0].text.split('\n'))
 clues_down.append(browser.find_elements_by_xpath('//*[@id="root"]/div/div/div[4]/div/main/div[2]/div/article/section[2]/div[2]/ol')[0].text.split('\n'))
 
-print(clues_across)
-print(clues_down)
+print("Across Clues\n"+clues_across)
+print("Down Clues\n"+clues_down)
 
 solutions = []
 
-#print("Audio bakalım",browser.find_elements_by_tag_name("audio"))
 
 for i in range(25):
     css_link = '#xwd-board > g:nth-child(5) > g:nth-child('+str(i+1)+')'
@@ -30,6 +32,6 @@ for i in range(25):
     else:
         solutions.append(browser.find_elements_by_css_selector(css_link)[0].text.split("\n"))
 
-print(solutions)  # Solutions are listed as increasing order from left to right and top to down                                                      
+print("Solutions\n"+solutions)  # Solutions are listed as increasing order from left to right and top to down                                                      
 browser.quit()
 
